915848dfbf3caf3af9c67e0eacd96deb563899e5
차성원 <sungwon4728@naver.com>
2024-04-22 15:41:19 +0900

    init: 프로젝트 생성

---
+++ backEnd/src/main/java/com/quiz/ourClass/OurClassApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.ourClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurClassApplication.class, args);
+	}
+
+}
---
+++ backEnd/src/test/java/com/quiz/ourClass/OurClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.ourClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
