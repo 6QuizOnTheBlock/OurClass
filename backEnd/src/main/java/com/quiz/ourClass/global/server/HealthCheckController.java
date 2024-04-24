e95c5ec902a407d1361ef6ed95862e2555d3ea6d
HABIN <dhdudgns80@naver.com>
2024-04-24 17:36:45 +0900

    devops : healthCheck Controller add
    Server healthCheck 를 위해서 Controller 추가하고 /health-check 엔드포인트에 OK 응답주는 API 추가했습니다.

---
+++ backEnd/src/main/java/com/quiz/ourClass/global/server/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.quiz.ourClass.global.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
+}
