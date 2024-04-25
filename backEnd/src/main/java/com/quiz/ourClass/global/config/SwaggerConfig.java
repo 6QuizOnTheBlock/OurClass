4ee7b490d3bb59c6e72f93c305c1efb0bd16454e
차성원 <sungwon4728@naver.com>
2024-04-25 10:15:02 +0900

    feat #8: Swagger 추가 및 설정

---
+++ backEnd/src/main/java/com/quiz/ourClass/global/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.quiz.ourClass.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+            .title("울반 - OurClass API문서")
+            .version("v0.0.1")
+            .description("OurClass API 명세서.");
+
+        SecurityScheme bearer = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("Authorization")
+            .in(SecurityScheme.In.HEADER)
+            .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("Authorization");
+
+        Components components = new Components()
+            .addSecuritySchemes("Authorization", bearer);
+
+        return new OpenAPI()
+            .components(components)
+            .addSecurityItem(addSecurityItem)
+            .info(info);
+    }
+}
