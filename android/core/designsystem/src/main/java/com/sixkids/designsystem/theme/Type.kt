05d21f6d318f50e4f10e4d1cffc186aff872642e
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-04-26 00:15:40 +0900

    fix #14: 안 쓰이는 import 제거

--- android/core/designsystem/src/main/java/com/sixkids/designsystem/theme/Type.kt
+++ android/core/designsystem/src/main/java/com/sixkids/designsystem/theme/Type.kt
@@ -1,10 +1,8 @@
 package com.sixkids.designsystem.theme
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
