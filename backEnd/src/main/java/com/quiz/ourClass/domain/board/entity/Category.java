b102ce5910be7ca076bd26752c309413aaa27769
차성원 <sungwon4728@naver.com>
2024-04-25 11:27:25 +0900

    fix: 카테고리 enum 값 추가.
    빈 enum사용으로 경고발생하여 미리 추가합니다

--- backEnd/src/main/java/com/quiz/ourClass/domain/board/entity/Category.java
+++ backEnd/src/main/java/com/quiz/ourClass/domain/board/entity/Category.java
@@ -1,4 +1,5 @@
 package com.quiz.ourClass.domain.board.entity;
 
 public enum Category {
+    NOTICE_BOARD, FREE_BOARD
 }
--- backEnd/src/main/java/com/quiz/ourClass/domain/notice/entity/NoticeType.java
+++ backEnd/src/main/java/com/quiz/ourClass/domain/notice/entity/NoticeType.java
@@ -1,4 +1,5 @@
 package com.quiz.ourClass.domain.notice.entity;
 
 public enum NoticeType {
+    TAG, RELAY, TALK
 }
