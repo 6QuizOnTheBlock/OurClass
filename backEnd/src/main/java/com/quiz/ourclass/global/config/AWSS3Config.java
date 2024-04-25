ba2eeba5ea08e0a280e40270bff3e1f4434031f7
HABIN <dhdudgns80@naver.com>
2024-04-25 13:36:26 +0900

    feat #11: S3 추가 및 이미지 업로더 추가, 관련 설정
    이미지 업로드, 삭제 추가 설정 등 설정했습니다. (테스트 완료)

---
+++ backEnd/src/main/java/com/quiz/ourclass/global/config/AWSS3Config.java
@@ -0,0 +1,40 @@
+package com.quiz.ourclass.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.quiz.ourclass.global.util.AwsS3ObjectStorage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSS3Config {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+            .build();
+    }
+
+    @Bean
+    public AwsS3ObjectStorage awsS3ObjectStorageUpload(AmazonS3 amazonS3) {
+        AwsS3ObjectStorage awsS3ObjectStorageUpload = new AwsS3ObjectStorage(amazonS3);
+        awsS3ObjectStorageUpload.setBucket(bucket);
+        return awsS3ObjectStorageUpload;
+    }
+}
