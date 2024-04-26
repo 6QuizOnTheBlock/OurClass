8f48e8700e7351df40b17eec5ba03cbaa66527be
정철주 <cjfwn96@gmail.com>
2024-04-26 17:26:30 +0900

    create: 네비게이션 기본 구성 클래스 생성

--- android/feature/navigator/src/main/java/com/sixkids/navigator/MainActivity.kt
+++ android/feature/navigator/src/main/java/com/sixkids/navigator/MainActivity.kt
@@ -3,14 +3,8 @@ package com.sixkids.navigator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.sixkids.designsystem.theme.UlbanTheme
+import com.sixkids.navigator.testscreens.home.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,30 +13,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UlbanTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UlbanTheme {
-        Greeting("Android")
     }
 }
---
+++ android/feature/navigator/src/main/java/com/sixkids/navigator/MainNav.kt
@@ -0,0 +1,9 @@
+package com.sixkids.navigator
+
+import androidx.navigation.NavController
+
+class MainNav(
+    val navController: NavController,
+) {
+
+}
