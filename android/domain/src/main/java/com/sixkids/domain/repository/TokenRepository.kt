fab669861c78851afa9c9d5e1dab7e137a059d30
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-04-27 20:21:46 +0900

    fet #19: Token 관련 Repository 추가
    TokenDataStore 삭제
    TokenRepository 추가
    Repository DI Module 추가

---
+++ android/domain/src/main/java/com/sixkids/domain/repository/TokenRepository.kt
@@ -0,0 +1,8 @@
+package com.sixkids.domain.repository
+
+interface TokenRepository {
+    suspend fun getAccessToken(): String
+    suspend fun saveAccessToken(token: String)
+    suspend fun getRefreshToken(): String
+    suspend fun saveRefreshToken(token: String)
+}
