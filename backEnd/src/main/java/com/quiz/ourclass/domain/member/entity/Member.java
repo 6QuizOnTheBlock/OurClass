db7e679ef756b0423a522be028acefa1e88c0ab7
JeonSooMin <wjsaos2081@gmail.com>
2024-04-29 02:51:07 +0900

    feat #22: JwtUtil 작성 및 Member.java 에 Getter Setter 달기

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/entity/Member.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/entity/Member.java
@@ -6,8 +6,12 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Member {
 
     @Id
