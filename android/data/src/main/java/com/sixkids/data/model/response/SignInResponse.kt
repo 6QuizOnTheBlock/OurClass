a6439b833ad77efb4554bbeba35aa3c5d3275ddf
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-04-29 15:25:52 +0900

    feat #36: response toModel 확장 함수 추가

--- android/data/src/main/java/com/sixkids/data/model/response/SignInResponse.kt
+++ android/data/src/main/java/com/sixkids/data/model/response/SignInResponse.kt
@@ -1,6 +1,13 @@
 package com.sixkids.data.model.response
 
+import com.sixkids.model.JwtToken
+
 data class SignInResponse(
     val accessToken : String,
     val refreshToken : String
+)
+
+internal fun SignInResponse.toModel() = JwtToken(
+    accessToken = accessToken,
+    refreshToken = refreshToken
 )
