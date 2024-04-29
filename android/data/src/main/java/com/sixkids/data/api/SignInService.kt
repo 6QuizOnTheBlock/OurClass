3f922ae9b256fefec10a4e97684c2c3bf4ad4ded
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-04-29 15:16:20 +0900

    feat #36: 로그인 api 구현
    Request, Response Data Class 추가
    SignInService 추가
    DI 추가

---
+++ android/data/src/main/java/com/sixkids/data/api/SignInService.kt
@@ -0,0 +1,15 @@
+package com.sixkids.data.api
+
+import com.sixkids.data.model.request.SignInRequest
+import com.sixkids.data.model.response.SignInResponse
+import com.sixkids.data.network.ApiResult
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface SignInService {
+
+    @POST("members/sign-in")
+    suspend fun signIn(
+        @Body signInRequest: SignInRequest
+    ): ApiResult<SignInResponse>
+}
