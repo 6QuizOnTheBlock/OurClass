ea5fd2725b091d3d7e226e5cca498be19d673060
차성원 <sungwon4728@naver.com>
2024-04-29 21:06:46 +0900

    feat #20: 에러코드 추가

--- backEnd/src/main/java/com/quiz/ourclass/global/exception/ErrorCode.java
+++ backEnd/src/main/java/com/quiz/ourclass/global/exception/ErrorCode.java
@@ -32,7 +32,11 @@ public enum ErrorCode {
     IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 찾을 수 없습니다."),
 
     //userAccess
-    MEMBER_NOT_IN_ORGANIZATION(HttpStatus.FORBIDDEN, "멤버가 해당 단체 소속이 아닙니다.");
+    MEMBER_NOT_IN_ORGANIZATION(HttpStatus.FORBIDDEN, "멤버가 해당 단체 소속이 아닙니다."),
+
+    //organization
+    ORGANIZATION_NOT_FOUND(HttpStatus.NOT_FOUND, "학급을 찾을 수 없습니다."),
+    ;
 
     private final HttpStatus status;
     private final String message;
