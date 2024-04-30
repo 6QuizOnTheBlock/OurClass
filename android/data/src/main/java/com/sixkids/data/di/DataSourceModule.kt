90163196b1b072f791fad5ffe72c19a889778033
이호성 <ghtjd8264@naver.com>
2024-04-30 13:31:12 +0900

    hotfix: DataSourceModule 추가

---
+++ android/data/src/main/java/com/sixkids/data/di/DataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.sixkids.data.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+
+}
