f5356434c48031a80ec7546bf2aa2e173d3b743b
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-04-30 11:10:23 +0900

    feat #36: side effect 를 collect하는 확장함수 추가

---
+++ android/core/ui/src/main/java/com/sixkids/ui/extension/Flow.kt
@@ -0,0 +1,22 @@
+package com.sixkids.ui.extension
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+
+@Composable
+inline fun <reified T> Flow<T>.collectWithLifecycle(
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
+    noinline action: suspend (T) -> Unit,
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
+            this@collectWithLifecycle.collect { action(it) }
+        }
+    }
+}
