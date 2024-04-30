f728e66d8eaa1001b1a0874b506f34d88fa63cba
정철주 <cjfwn96@gmail.com>
2024-04-30 15:55:10 +0900

    feat: 교사 게시판 탭 메인 화면 구현

---
+++ android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/main/BoardMainContract.kt
@@ -0,0 +1,6 @@
+package com.sixkids.teacher.board.main
+
+data class BoardMainState(
+    val isLoading: Boolean = false,
+    val classString: String = "",
+)
--- android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/main/BoardMainScreen.kt
+++ android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/main/BoardMainScreen.kt
@@ -1,15 +1,32 @@
 package com.sixkids.teacher.board.main
 
+import android.util.LayoutDirection
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+
 import androidx.compose.ui.unit.dp
+import androidx.core.text.layoutDirection
+import com.sixkids.designsystem.theme.Blue
+import com.sixkids.designsystem.theme.Orange
+import com.sixkids.designsystem.theme.UlbanTypography
+import com.sixkids.designsystem.theme.component.card.ContentAligment
+import com.sixkids.designsystem.theme.component.card.ContentCard
+import com.sixkids.teacher.board.R
+import java.util.Locale
+import com.sixkids.designsystem.R as UlbanRes
 
 @Composable
 fun BoardMainRoute(
@@ -26,14 +43,49 @@ fun BoardMainRoute(
 
 @Composable
 fun BoardMainScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    boardMainState: BoardMainState = BoardMainState()
 ) {
     Column(
         modifier = modifier
             .fillMaxSize()
             .padding(20.dp)
     ) {
-        Text(text = "Board Main Screen")
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            text = stringResource(id = R.string.board_main_title),
+            style = UlbanTypography.titleLarge
+        )
+        Text(
+            // TODO text = boardMainState.classString,
+            text = "인동초등학교 3학년 1반",
+            style = UlbanTypography.bodySmall
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ContentCard(
+            modifier = Modifier.padding(start = 40.dp),
+            imageModifier = Modifier.rotate(-20f),
+            contentName = stringResource(id = R.string.board_main_announce),
+            contentImageId = UlbanRes.drawable.announce,
+            cardColor = Orange,
+            contentAligment = ContentAligment.ImageStart_TextEnd
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ContentCard(
+            modifier = Modifier.padding(end = 40.dp),
+            contentName = stringResource(id = R.string.board_main_post),
+            contentImageId = UlbanRes.drawable.board,
+            cardColor = Blue,
+            contentAligment = ContentAligment.ImageStart_TextEnd
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        ContentCard(
+            modifier = Modifier.padding(start = 40.dp),
+            contentName = stringResource(id = R.string.board_main_chat),
+            contentImageId = UlbanRes.drawable.chat,
+            cardColor = Orange,
+            contentAligment = ContentAligment.ImageStart_TextEnd
+        )
     }
 }
 
@@ -41,5 +93,7 @@ fun BoardMainScreen(
 @Preview(showBackground = true)
 @Composable
 fun BoardMainScreenPreview() {
-    BoardMainScreen()
+    BoardMainScreen(
+        boardMainState = BoardMainState(classString = "구미초등학교 1학년 1반")
+    )
 }
