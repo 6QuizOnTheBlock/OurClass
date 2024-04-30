0692d05231ef8c01e0055e49752f2715806eefad
이호성 <ghtjd8264@naver.com>
2024-04-30 16:09:11 +0900

    feat #61: 챌린지 목록 조회 페이징 요청 로직 구현

--- android/data/src/main/java/com/sixkids/data/repository/challenge/ChallengeRepositoryImpl.kt
+++ android/data/src/main/java/com/sixkids/data/repository/challenge/ChallengeRepositoryImpl.kt
@@ -1,14 +1,32 @@
 package com.sixkids.data.repository.challenge
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.sixkids.data.api.ChallengeService
+import com.sixkids.data.repository.challenge.remote.ChallengeHistoryPagingSource
+import com.sixkids.data.repository.challenge.remote.ChallengeHistoryPagingSource.Companion.DEFAULT_SIZE
 import com.sixkids.domain.repository.ChallengeRepository
 import com.sixkids.model.Challenge
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ChallengeRepositoryImpl @Inject constructor(
-
+    private val challengeService: ChallengeService,
 ) : ChallengeRepository {
-    override suspend fun getChallengeHistory(): List<Challenge> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getChallengeHistory(
+        organizationId: Int,
+        memberId: Int?,
+    ): Flow<PagingData<Challenge>> =
+        Pager(
+            config = PagingConfig(DEFAULT_SIZE),
+            pagingSourceFactory = {
+                ChallengeHistoryPagingSource(
+                    challengeService,
+                    organizationId,
+                    memberId,
+                )
+            }
+        ).flow
 
 }
--- android/domain/src/main/java/com/sixkids/domain/repository/ChallengeRepository.kt
+++ android/domain/src/main/java/com/sixkids/domain/repository/ChallengeRepository.kt
@@ -1,9 +1,12 @@
 package com.sixkids.domain.repository
 
+import androidx.paging.PagingData
 import com.sixkids.model.Challenge
+import kotlinx.coroutines.flow.Flow
 
 interface ChallengeRepository {
-
-    suspend fun getChallengeHistory(): List<Challenge>
-
+    suspend fun getChallengeHistory(
+        organizationId: Int,
+        memberId: Int?
+    ): Flow<PagingData<Challenge>>
 }
