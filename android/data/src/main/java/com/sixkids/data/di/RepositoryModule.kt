965e84f91084aa321cbdd781e11b2f9ee24484e5
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-01 17:41:49 +0900

    feat #36: UserRepository DI

--- android/data/src/main/java/com/sixkids/data/di/RepositoryModule.kt
+++ android/data/src/main/java/com/sixkids/data/di/RepositoryModule.kt
@@ -1,18 +1,28 @@
 package com.sixkids.data.di
 
 import com.sixkids.data.repository.TokenRepositoryImpl
+import com.sixkids.data.repository.user.UserRepositoryImpl
 import com.sixkids.domain.repository.TokenRepository
+import com.sixkids.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
 
+    @Singleton
     @Binds
     abstract fun bindTokenRepository(
         tokenRepository: TokenRepositoryImpl
     ): TokenRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindUserRepository(
+        userRepository: UserRepositoryImpl
+    ): UserRepository
 }
