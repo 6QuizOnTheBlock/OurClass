03c683d036fc7554673834d1e52d6c1e16d61baa
HABIN <dhdudgns80@naver.com>
2024-05-02 09:21:18 +0900

    feat #86: 게시글 상세조회 updateTime 추가
    1. 게시글 상세조회 부분에 게시글, 댓글 updateTime 추가했습니다.

--- backEnd/src/main/java/com/quiz/ourclass/domain/board/dto/CommentChildrenDTO.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/board/dto/CommentChildrenDTO.java
@@ -13,7 +13,9 @@ public record CommentChildrenDTO(
     @Schema(description = "자식 댓글 내용", example = "아..나도 속았다ㅠㅠ 우리끼리 가실?")
     String content,
     @Schema(description = "자식 댓글 작성 시간", example = "2024-04-29T14:30:00")
-    LocalDateTime time,
+    LocalDateTime createTime,
+    @Schema(description = "자식 댓글 수정 시간", example = "2024-04-30T21:30:00")
+    LocalDateTime updateTime,
     @Schema(description = "부모 댓글 PK")
     Long parentId
 ) {
--- backEnd/src/main/java/com/quiz/ourclass/domain/board/dto/response/PostDetailResponse.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/board/dto/response/PostDetailResponse.java
@@ -9,9 +9,12 @@ import java.util.List;
 
 @Schema(description = "게시글 상세 정보 반환 DTO")
 public record PostDetailResponse(
+    @Schema(description = "게시글 작성자 정보")
     MemberSimpleDTO member,
     @Schema(description = "게시글 작성 시간", example = "2024-04-29T13:00:00")
     LocalDateTime createTime,
+    @Schema(description = "게시글 수정 시간", example = "2024-04-29T13:00:00")
+    LocalDateTime updateTime,
     @Schema(description = "게시글 제목", example = "오늘 나랑 같이 PC방 갈 사람!!!")
     String title,
     @Schema(description = "게시글 내용", example = "구라지롱~")
