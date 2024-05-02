72f097c82e822135f8abcdce87170f52b9519a32
JeonSooMin <wjsaos2081@gmail.com>
2024-05-02 09:04:50 +0900

    feat #22: 성원이 피드백대로 고치기

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/dto/OIDCPublicKeyDTO.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/dto/OIDCPublicKeyDTO.java
@@ -6,9 +6,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class OIDCPublicKeyDTO {
-    private String kid;
-    private String alg;
-    private String use;
-    private String n;
-    private String e;
+    private String kid;     // 공개키 ID
+    private String alg;     // 공개 키 암호화에 쓰여진 알고리즘
+    private String use;     // 공개키의 용도 sig(서명)으로 고정
+    private String n;       // 공개키 모듈 (공개키는 n과 e 쌍으로 구성됨)
+    private String e;       // 공개키 지수 (공개키는 n과 e 쌍으로 구성됨)
 }
