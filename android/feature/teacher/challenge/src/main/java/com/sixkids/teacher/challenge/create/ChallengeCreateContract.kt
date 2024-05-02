82b1f467eb6178420da0f12a1da3c163053c74c1
이호성 <ghtjd8264@naver.com>
2024-05-02 23:20:23 +0900

    feat #101: 챌린지 생성 화면 이동

---
+++ android/feature/teacher/challenge/src/main/java/com/sixkids/teacher/challenge/create/ChallengeCreateContract.kt
@@ -0,0 +1,23 @@
+package com.sixkids.teacher.challenge.create
+
+import androidx.annotation.StringRes
+import com.sixkids.ui.base.SideEffect
+import com.sixkids.ui.base.UiState
+
+data class ChallengeCreateUiState(
+    val isLoading: Boolean = false,
+    val buttonEnabled: Boolean = false,
+    @StringRes val buttonText: Int? = null,
+    val step: ChallengeCreateStep = ChallengeCreateStep.INFO,
+): UiState
+
+sealed interface ChallengeCreateEffect: SideEffect {
+}
+
+enum class ChallengeCreateStep {
+    INFO,
+    GROUP_TYPE,
+    MATCHING_TYPE,
+    CREATE,
+    RESULT,
+}
