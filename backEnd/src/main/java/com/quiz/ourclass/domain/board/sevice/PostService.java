dba9b5edc535cc0f8e71c1ea653636daf5cf03f0
HABIN <dhdudgns80@naver.com>
2024-05-02 16:56:22 +0900

    feat #92: 게시글 쓰기 알림장 수정, 게시글 삭제 권한 로직 변경
    1. 게시글 쓰기 부분에 학생이 알림장을 작성하는 부분에서 Exception 처리 했습니다.
    2. 게시글 삭제 부분에서 권한 검증하는 부분을 수정했습니다.(댓글 삭제와 동일)

--- backEnd/src/main/java/com/quiz/ourclass/domain/board/sevice/PostService.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/board/sevice/PostService.java
@@ -10,9 +10,9 @@ public interface PostService {
 
     Long write(Long organizationId, MultipartFile file, PostRequest request);
 
-    Long modify(Long id, MultipartFile file, UpdatePostRequest request);
+    Long modify(Long postId, MultipartFile file, UpdatePostRequest request);
 
-    Boolean delete(Long id);
+    Boolean delete(Long postId);
 
-    PostDetailResponse detailView(Long id);
+    PostDetailResponse detailView(Long postId);
 }
