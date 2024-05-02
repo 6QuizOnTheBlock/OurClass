9cbcbbabfba967aeea9cca86bd750d6b1c7ebbe2
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-02 11:20:22 +0900

    feat #88: jwt 토큰 저장

--- android/data/src/main/java/com/sixkids/data/repository/user/UserRepositoryImpl.kt
+++ android/data/src/main/java/com/sixkids/data/repository/user/UserRepositoryImpl.kt
@@ -1,15 +1,27 @@
 package com.sixkids.data.repository.user
 
+import android.util.Log
 import com.sixkids.data.repository.user.remote.UserDataSource
+import com.sixkids.domain.repository.TokenRepository
 import com.sixkids.domain.repository.UserRepository
 import com.sixkids.model.JwtToken
 import javax.inject.Inject
 
+private const val TAG = "D107"
 class UserRepositoryImpl @Inject constructor(
-    private val userDataSource: UserDataSource
+    private val userDataSource: UserDataSource,
+    private val tokenRepository: TokenRepository
 ) : UserRepository{
     override suspend fun signIn(idToken: String): JwtToken {
-        return userDataSource.signIn(idToken)
-    }
+        try {
+            val response = userDataSource.signIn(idToken)
 
+            tokenRepository.saveAccessToken(response.accessToken)
+            tokenRepository.saveRefreshToken(response.refreshToken)
+
+            return response
+        }catch (e: Exception){
+            throw e
+        }
+    }
 }
