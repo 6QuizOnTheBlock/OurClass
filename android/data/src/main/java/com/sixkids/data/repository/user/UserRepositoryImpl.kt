150a7176ad9f8ad67b67e01f8f0ace9ae27d0bab
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-02 14:19:55 +0900

    fix #88: 코드 정리

--- android/data/src/main/java/com/sixkids/data/repository/user/UserRepositoryImpl.kt
+++ android/data/src/main/java/com/sixkids/data/repository/user/UserRepositoryImpl.kt
@@ -1,13 +1,11 @@
 package com.sixkids.data.repository.user
 
-import android.util.Log
 import com.sixkids.data.repository.user.remote.UserDataSource
 import com.sixkids.domain.repository.TokenRepository
 import com.sixkids.domain.repository.UserRepository
 import com.sixkids.model.JwtToken
 import javax.inject.Inject
 
-private const val TAG = "D107"
 class UserRepositoryImpl @Inject constructor(
     private val userDataSource: UserDataSource,
     private val tokenRepository: TokenRepository
