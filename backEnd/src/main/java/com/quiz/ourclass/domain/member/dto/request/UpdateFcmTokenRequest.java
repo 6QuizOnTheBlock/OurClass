0604600c973353fad7c77df25cd3e180f804519a
HABIN <dhdudgns80@naver.com>
2024-05-03 16:29:53 +0900

    feat: 사용자 FCM 토큰 저장 및 수정 기능 Request Schema 추가
    Schema 추가 했습니다.

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/dto/request/UpdateFcmTokenRequest.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/dto/request/UpdateFcmTokenRequest.java
@@ -1,6 +1,9 @@
 package com.quiz.ourclass.domain.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UpdateFcmTokenRequest(
+    @Schema(description = "FCM 토큰", example = "zxcvoiuwaerlknsdlkfnklasdf/111asjndkl123dasd")
     String fcmToken
 ) {
 
