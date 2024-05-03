33a8c0045b3b605e279d7a16d1f4eacb82b91abb
HABIN <dhdudgns80@naver.com>
2024-05-03 16:28:03 +0900

    feat: 사용자 FCM 토큰 저장 및 수정 기능 Swagger 추가
    Swagger 추가 했습니다.

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/controller/docs/MemberControllerDocs.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/controller/docs/MemberControllerDocs.java
@@ -1,16 +1,17 @@
 package com.quiz.ourclass.domain.member.controller.docs;
 
-import com.quiz.ourclass.domain.member.dto.TokenDTO;
 import com.quiz.ourclass.domain.member.dto.request.MemberSignInRequest;
 import com.quiz.ourclass.domain.member.dto.request.MemberSignUpRequest;
+import com.quiz.ourclass.domain.member.dto.request.UpdateFcmTokenRequest;
 import com.quiz.ourclass.global.dto.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "MemberController", description = "회원관리 API")
 public interface MemberControllerDocs {
@@ -22,9 +23,9 @@ public interface MemberControllerDocs {
                 description = "회원가입에 성공하였습니다."
             ),
             @ApiResponse(responseCode = "404",
-            description = "기존에 존재하는 회원입니다."),
+                description = "기존에 존재하는 회원입니다."),
             @ApiResponse(responseCode = "503",
-            description = "OIDC 토큰 인증에 실패했습니다.")
+                description = "OIDC 토큰 인증에 실패했습니다.")
         })
     @PostMapping
     ResponseEntity<ResultResponse<?>> signUp(MemberSignUpRequest request);
@@ -42,5 +43,16 @@ public interface MemberControllerDocs {
                 description = "OIDC 토큰 인증에 실패했습니다.")
         })
     @PostMapping
-    ResponseEntity<ResultResponse<?>>  signIn (MemberSignInRequest request);
+    ResponseEntity<ResultResponse<?>> signIn(MemberSignInRequest request);
+
+    @Operation(summary = "FCM 토큰 저장 및 갱신",
+        description = "입력으로 들어오는 FCM 토큰을 저장 및 갱신 합니다.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "(message : \"Success\")")
+        }
+    )
+    @PostMapping("/fcm")
+    ResponseEntity<ResultResponse<Void>> saveFcmToken(
+        @Parameter(name = "request", description = "FCM 토큰", required = true, in = ParameterIn.DEFAULT)
+        @RequestBody UpdateFcmTokenRequest request);
 }
