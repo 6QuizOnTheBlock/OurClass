1d322b648538578a08211794707400e77d79826d
정철주 <cjfwn96@gmail.com>
2024-05-04 19:54:40 +0900

    feat #102: 게시글 목록 타이틀 스타일 개선

--- android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/post/component/PostItem.kt
+++ android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/post/component/PostItem.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sixkids.designsystem.theme.UlbanTypography
@@ -32,7 +33,9 @@ fun PostItem(
     ) {
         Text(
             text = title,
-            style = UlbanTypography.titleLarge
+            style = UlbanTypography.titleLarge,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
         )
         Spacer(modifier = Modifier.height(10.dp))
         Row(
