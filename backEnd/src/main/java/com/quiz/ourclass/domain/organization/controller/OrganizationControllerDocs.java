9d71b38215f8809756a63fb014ede57a218b573d
차성원 <sungwon4728@naver.com>
2024-05-04 23:38:31 +0900

    feat #121: 학급 목록 조회 스웨거 추가

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/OrganizationControllerDocs.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/OrganizationControllerDocs.java
@@ -1,6 +1,7 @@
 package com.quiz.ourclass.domain.organization.controller;
 
 import com.quiz.ourclass.domain.organization.dto.OrganizationRequest;
+import com.quiz.ourclass.domain.organization.dto.OrganizationResponse;
 import com.quiz.ourclass.global.dto.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -25,4 +27,12 @@ public interface OrganizationControllerDocs {
         @RequestBody
         OrganizationRequest organizationRequest
     );
+
+    @Operation(summary = "학급 목록 조회",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "(message : \"Success\")",
+                content = @Content(schema = @Schema(implementation = OrganizationResponse.class)))
+        })
+    @GetMapping
+    ResponseEntity<ResultResponse<?>> getOrganizations();
 }
--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/OrganizationResponse.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/OrganizationResponse.java
@@ -1,11 +1,17 @@
 package com.quiz.ourclass.domain.organization.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
+@Schema(description = "학급 조회 응답 DTO")
 public record OrganizationResponse(
+    @Schema(description = "학급 id")
     long id,
+    @Schema(description = "학급 이름")
     String name,
+    @Schema(description = "학급 멤버 수")
     int memberCount,
+    @Schema(description = "학급 생성 날짜")
     LocalDate createTime
 ) {
 
