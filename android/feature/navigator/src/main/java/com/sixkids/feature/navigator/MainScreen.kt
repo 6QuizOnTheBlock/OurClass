c94a427d07a4948cd4fc20e2f528d4e8bc5dbefd
정철주 <cjfwn96@gmail.com>
2024-05-04 17:15:44 +0900

    feat #102: 자유게시판 이동 구현

--- android/feature/navigator/src/main/java/com/sixkids/feature/navigator/MainScreen.kt
+++ android/feature/navigator/src/main/java/com/sixkids/feature/navigator/MainScreen.kt
@@ -81,6 +81,7 @@ fun MainScreen(
 
             boardNavGraph(
                 padding = innerPadding,
+                navigateToPost = navigator::navigatePost,
             )
 
             challengeNavGraph(
--- android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/navigation/BoardNavigation.kt
+++ android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/navigation/BoardNavigation.kt
@@ -6,19 +6,33 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.sixkids.teacher.board.main.BoardMainRoute
+import com.sixkids.teacher.board.post.PostRoute
 
 fun NavController.navigateBoard(navOptions: NavOptions) {
     navigate(BoardRoute.defaultRoute, navOptions)
 }
 
+fun NavController.navigatePost() {
+    navigate(BoardRoute.postRoute)
+}
+
 fun NavGraphBuilder.boardNavGraph(
     padding: PaddingValues,
+    navigateToPost: () -> Unit,
 ) {
     composable(route = BoardRoute.defaultRoute){
-        BoardMainRoute(padding)
+        BoardMainRoute(
+            padding = padding,
+            navigateToPost = navigateToPost
+        )
+    }
+
+    composable(BoardRoute.postRoute){
+        PostRoute(padding)
     }
 }
 
 object BoardRoute {
     const val defaultRoute = "board"
+    const val postRoute = "post"
 }
