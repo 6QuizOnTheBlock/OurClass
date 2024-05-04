d85c8d7b45cb61fe6f6d8b75e8bbda995ec3f355
차성원 <sungwon4728@naver.com>
2024-05-04 19:42:11 +0900

    feat #121: 학급 목록 조회 api 구현

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/repository/MemberOrganizationRepository.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/repository/MemberOrganizationRepository.java
@@ -1,10 +1,14 @@
 package com.quiz.ourclass.domain.organization.repository;
 
+import com.quiz.ourclass.domain.member.entity.Member;
 import com.quiz.ourclass.domain.organization.entity.MemberOrganization;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberOrganizationRepository extends JpaRepository<MemberOrganization, Long> {
+
+    List<MemberOrganization> findByMember(Member member);
 
     Optional<MemberOrganization> findByMemberIdAndOrganizationId(Long memberId,
         Long organizationId);
--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/service/OrganizationService.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/service/OrganizationService.java
@@ -1,8 +1,12 @@
 package com.quiz.ourclass.domain.organization.service;
 
 import com.quiz.ourclass.domain.organization.dto.OrganizationRequest;
+import com.quiz.ourclass.domain.organization.dto.OrganizationResponse;
+import java.util.List;
 
 public interface OrganizationService {
 
     Long createOrganization(OrganizationRequest organizationRequest);
+
+    List<OrganizationResponse> getOrganizations();
 }
