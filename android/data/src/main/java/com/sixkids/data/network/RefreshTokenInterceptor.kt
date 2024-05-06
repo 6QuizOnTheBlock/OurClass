c181ad9842e73c4a10888925d1c0e58b8bb6e167
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-07 01:15:09 +0900

    fix #162: 토큰 재발급 Authenticator 수정

--- android/data/src/main/java/com/sixkids/data/network/RefreshTokenInterceptor.kt
+++ android/data/src/main/java/com/sixkids/data/network/RefreshTokenInterceptor.kt
@@ -11,15 +11,20 @@ class RefreshTokenInterceptor @Inject constructor(
 ) : Interceptor {
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
+        const val REFRESH_HEADER = "refreshToken"
         const val TOKEN_TYPE = "Bearer"
     }
 
     override fun intercept(chain: Interceptor.Chain): Response {
-        val token = runBlocking {
+        val atk = runBlocking {
+            tokenRepository.getAccessToken()
+        }
+        val rtk = runBlocking {
             tokenRepository.getRefreshToken()
         }
         val request = chain.request().newBuilder().apply {
-            addHeader(AUTHORIZATION_HEADER, "$TOKEN_TYPE $token")
+            addHeader(AUTHORIZATION_HEADER, "$TOKEN_TYPE $atk")
+            addHeader(AUTHORIZATION_HEADER, "$REFRESH_HEADER $rtk")
         }
 
         return chain.proceed(request.build())
