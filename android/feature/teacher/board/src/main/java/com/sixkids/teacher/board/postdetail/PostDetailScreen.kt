3b92bd0763bae64312f15ac2b963c851e9b59474
정철주 <cjfwn96@gmail.com>
2024-05-07 03:02:22 +0900

    create #102: 게시글 상세 조회 컴포넌트 생성

---
+++ android/feature/teacher/board/src/main/java/com/sixkids/teacher/board/postdetail/PostDetailScreen.kt
@@ -0,0 +1,13 @@
+package com.sixkids.teacher.board.postdetail
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PostDetailRoute() {
+
+}
+
+@Composable
+fun PostDetailScreen() {
+
+}
