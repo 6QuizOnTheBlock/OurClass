bb7ce9a36b3001cad68c772c3877e402252de8b7
정철주 <cjfwn96@gmail.com>
2024-05-08 19:45:33 +0900

    feat #170: PostRemoteDataSourceImpl 구현

--- android/data/src/main/java/com/sixkids/data/repository/post/remote/PostRemoteDataSourceImpl.kt
+++ android/data/src/main/java/com/sixkids/data/repository/post/remote/PostRemoteDataSourceImpl.kt
@@ -1,4 +1,72 @@
 package com.sixkids.data.repository.post.remote
 
-class PostRemoteDataSourceImpl {
+import com.sixkids.data.api.PostService
+import com.sixkids.data.model.request.NewPostRequest
+import com.sixkids.data.model.response.PostDetailResponse
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import java.io.File
+import javax.inject.Inject
+
+class PostRemoteDataSourceImpl @Inject constructor(
+    private val postService: PostService,
+): PostRemoteDataSource {
+    override suspend fun createPost(
+        organizationId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?
+    ): Long {
+        val image = file?.asRequestBody("image/jpg".toMediaTypeOrNull())
+        val multipartImage = image?.let {
+            MultipartBody.Part.createFormData("file", file.name, it)
+        }
+
+        val request = NewPostRequest(
+            title = title,
+            content = content,
+            secretStatus = secretStatus,
+            postCategory = postCategory,
+        )
+
+        return postService.createPost(organizationId, request, multipartImage).getOrThrow().data
+    }
+
+    override suspend fun getPostDetail(postId: Long): PostDetailResponse {
+        return postService.getPostDetail(postId).getOrThrow().data
+    }
+
+    override suspend fun deletePost(postId: Long): Boolean {
+        return postService.deletePost(postId).getOrThrow().data
+    }
+
+    override suspend fun updatePost(
+        postId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?
+    ): Long {
+        val image = file?.asRequestBody("image/jpg".toMediaTypeOrNull())
+        val multipartImage = image?.let {
+            MultipartBody.Part.createFormData("file", file.name, it)
+        }
+
+        val request = NewPostRequest(
+            title = title,
+            content = content,
+            secretStatus = secretStatus,
+            postCategory = postCategory,
+        )
+
+        return postService.updatePost(postId, request, multipartImage).getOrThrow().data
+    }
+
+    override suspend fun reportPost(postId: Long): Boolean {
+        return postService.reportPost(postId).getOrThrow().data
+    }
 }
