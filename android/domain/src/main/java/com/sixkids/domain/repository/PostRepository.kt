450e7f9a20b649a2e7e13ed6196f0eb1353e4b32
정철주 <cjfwn96@gmail.com>
2024-05-08 16:16:57 +0900

    feat #170: 자유게시판 조회 repository 작성

---
+++ android/domain/src/main/java/com/sixkids/domain/repository/PostRepository.kt
@@ -0,0 +1,47 @@
+package com.sixkids.domain.repository
+
+import androidx.paging.PagingData
+import com.sixkids.model.Post
+import com.sixkids.model.PostDetail
+import kotlinx.coroutines.flow.Flow
+import java.io.File
+
+interface PostRepository {
+    suspend fun getPosts(
+        organizationId: Int,
+        memberId: Int,
+        page: Int,
+        size: Int,
+        postCategory: String,
+    ): Flow<PagingData<Post>>
+
+    suspend fun createPost(
+        organizationId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?,
+    ): Long
+
+    suspend fun getPostDetail(
+        postId: Long,
+    ): PostDetail
+
+    suspend fun deletePost(
+        postId: Long,
+    ): Boolean
+
+    suspend fun updatePost(
+        postId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?,
+    ): Long
+
+    suspend fun reportPost(
+        postId: Long,
+    ): Boolean
+}
