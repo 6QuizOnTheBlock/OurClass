79fb085e094d16273056434e5a624b2aa2630f84
차성원 <sungwon4728@naver.com>
2024-05-08 15:35:19 +0900

    docs #188: 학급 정보 수정 api 스웨거 추가

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/response/UpdateOrganizationResponse.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/response/UpdateOrganizationResponse.java
@@ -1,9 +1,12 @@
 package com.quiz.ourclass.domain.organization.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "학급 정보 수정 응답 DTO")
 public record UpdateOrganizationResponse(
+    @Schema(description = "학급 이름")
     String name
 ) {
 
