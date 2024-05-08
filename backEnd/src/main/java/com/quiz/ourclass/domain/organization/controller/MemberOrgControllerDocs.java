a8917879a7b11bcc1ee495a5324535569abe8787
차성원 <sungwon4728@naver.com>
2024-05-08 21:54:57 +0900

    docs #194: 멤버간 관계 조회 스웨거 추가

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/MemberOrgControllerDocs.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/MemberOrgControllerDocs.java
@@ -1,6 +1,8 @@
 package com.quiz.ourclass.domain.organization.controller;
 
+import com.quiz.ourclass.domain.organization.dto.request.RelationRequest;
 import com.quiz.ourclass.domain.organization.dto.request.UpdateExpRequest;
+import com.quiz.ourclass.domain.organization.dto.response.RelationResponse;
 import com.quiz.ourclass.domain.organization.dto.response.UpdateExpResponse;
 import com.quiz.ourclass.global.dto.ResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -11,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,5 +35,19 @@ public interface MemberOrgControllerDocs {
         long id,
         @RequestBody
         UpdateExpRequest updateExpRequest
+    );
+
+    @Operation(summary = "학급 멤버 관계 조회",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "(message : \"Success\")",
+                content = @Content(schema = @Schema(implementation = RelationResponse.class))),
+            @ApiResponse(responseCode = "404", description = "(message : \"두 멤버간의 관계를 찾을 수 없습니다.\")", content = @Content)
+        })
+    @GetMapping("/{id}/relation")
+    ResponseEntity<ResultResponse<?>> getMemberRelation(
+        @PathVariable
+        @Parameter(description = "학급 ID", required = true, in = ParameterIn.PATH)
+        long id,
+        RelationRequest relationRequest
     );
 }
