5e30a53b883db8148f4625afeb3434f66796568a
차성원 <sungwon4728@naver.com>
2024-05-08 20:53:00 +0900

    docs #195: 멤버 포인트 수정 스웨거 추가

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/MemberOrgControllerDocs.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/controller/MemberOrgControllerDocs.java
@@ -1,8 +1,36 @@
 package com.quiz.ourclass.domain.organization.controller;
 
+import com.quiz.ourclass.domain.organization.dto.request.UpdateExpRequest;
+import com.quiz.ourclass.domain.organization.dto.response.UpdateExpResponse;
+import com.quiz.ourclass.global.dto.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "MemberOrgControllerDocs", description = "회원관리 API")
 public interface MemberOrgControllerDocs {
 
+    @Operation(summary = "학급 멤버 포인트 수정",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "(message : \"Success\")",
+                content = @Content(schema = @Schema(implementation = UpdateExpResponse.class))),
+            @ApiResponse(responseCode = "403", description = "(message : \"해당 학급의 관리자가 아닙니다.\")", content = @Content),
+            @ApiResponse(responseCode = "404", description = "(message : \"학급 멤버를 찾을 수 없습니다.\")", content = @Content)
+        })
+    @PatchMapping("/{id}/point")
+    ResponseEntity<ResultResponse<?>> updateMemberExp(
+        @PathVariable
+        @Parameter(description = "학급 ID", required = true, in = ParameterIn.PATH)
+        long id,
+        @RequestBody
+        UpdateExpRequest updateExpRequest
+    );
 }
--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/response/UpdateExpResponse.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/response/UpdateExpResponse.java
@@ -1,9 +1,12 @@
 package com.quiz.ourclass.domain.organization.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "멤버 포인트 수정 응답 DTO")
 public record UpdateExpResponse(
+    @Schema(description = "포인트")
     int exp
 ) {
 
