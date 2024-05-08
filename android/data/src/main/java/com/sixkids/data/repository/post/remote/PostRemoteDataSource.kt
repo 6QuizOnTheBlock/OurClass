cd9e52e7328e1d5c0d3530cbc566f3b49fb1419f
정철주 <cjfwn96@gmail.com>
2024-05-08 19:35:41 +0900

    create #170: PostRemoteDataSource

--- android/data/src/main/java/com/sixkids/data/repository/post/remote/PostRemoteDataSource.kt
+++ android/data/src/main/java/com/sixkids/data/repository/post/remote/PostRemoteDataSource.kt
@@ -1,4 +1,38 @@
 package com.sixkids.data.repository.post.remote
 
+import com.sixkids.data.model.response.PostDetailResponse
+import java.io.File
+
 interface PostRemoteDataSource {
+
+    suspend fun createPost(
+        organizationId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?,
+    ): Long
+
+    suspend fun getPostDetail(
+        postId: Long,
+    ): PostDetailResponse
+
+    suspend fun deletePost(
+        postId: Long,
+    ): Boolean
+
+    suspend fun updatePost(
+        postId: Long,
+        title: String,
+        content: String,
+        secretStatus: Boolean,
+        postCategory: String,
+        file: File?,
+    ): Long
+
+    suspend fun reportPost(
+        postId: Long,
+    ): Boolean
+
 }
