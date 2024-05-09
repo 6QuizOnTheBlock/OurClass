66fd8378d938ae53840e8f0a386cb8f08e64e5ff
이호성 <ghtjd8264@naver.com>
2024-05-09 20:01:57 +0900

    feat #199 : 블루투스 광고(본인 검색 가능) 기능 구현

---
+++ android/core/bluetooth/src/main/kotlin/com/sixkids/core/bluetooth/BluetoothServer.kt
@@ -0,0 +1,70 @@
+package com.sixkids.core.bluetooth
+
+import android.Manifest
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.content.Context
+import androidx.annotation.RequiresPermission
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+class BluetoothServer(context: Context) {
+    private val bluetooth = context.getSystemService(Context.BLUETOOTH_SERVICE)
+            as? BluetoothManager ?: throw Exception("This device doesn't support Bluetooth")
+
+    private var advertiseCallback: AdvertiseCallback? = null
+
+
+    @RequiresPermission(allOf = [Manifest.permission.BLUETOOTH_ADVERTISE, Manifest.permission.BLUETOOTH_CONNECT])
+    suspend fun startAdvertising() {
+        val advertiser: BluetoothLeAdvertiser = bluetooth.adapter.bluetoothLeAdvertiser
+            ?: throw Exception("This device doesn't support Bluetooth advertising")
+
+        //if already advertising, ignore
+        if (advertiseCallback != null) {
+            return
+        }
+
+        bluetooth.adapter.name = "블루투스 테스트"
+
+        val settings = AdvertiseSettings.Builder()
+            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+            .setConnectable(true)
+            .setTimeout(0)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+            .build()
+
+        val data = AdvertiseData.Builder()
+            .setIncludeDeviceName(true)
+            .setIncludeTxPowerLevel(true)
+            .build()
+
+        advertiseCallback = suspendCoroutine { continuation ->
+            val advertiseCallback = object : AdvertiseCallback() {
+                override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
+                    continuation.resume(this)
+                }
+
+                override fun onStartFailure(errorCode: Int) {
+                    super.onStartFailure(errorCode)
+                    throw Exception("Unable to start advertising, errorCode: $errorCode")
+                }
+            }
+            advertiser.startAdvertising(settings, data, advertiseCallback)
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_ADVERTISE)
+    private fun stopAdvertising() {
+        val advertiser: BluetoothLeAdvertiser = bluetooth.adapter.bluetoothLeAdvertiser
+            ?: throw Exception("This device doesn't support Bluetooth advertising")
+
+        advertiseCallback?.let {
+            advertiser.stopAdvertising(it)
+            advertiseCallback = null
+        }
+    }
+}
