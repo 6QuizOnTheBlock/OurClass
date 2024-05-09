4795830ee52dc0e5a5e25b63a7f8816c8a795042
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-09 22:41:39 +0900

    fix #211: Chat data class 변수명 수정

--- android/core/model/src/main/java/com/sixkids/model/Chat.kt
+++ android/core/model/src/main/java/com/sixkids/model/Chat.kt
@@ -1,11 +1,10 @@
 package com.sixkids.model
 
 data class Chat(
-    val id: String,
-    val roomId: Long,
+    val roomId : Long,
     val memberId: Long,
     val memberName: String,
-    val memberProfilePhoto: String,
+    val memberImageUrl: String,
     val content: String,
     val sendDateTime: Long,
     val readCount: Int = 1
