6a943b7163d6bc7f93fc2b1529971136081d0d6a
이호성 <ghtjd8264@naver.com>
2024-05-10 02:15:03 +0900

    feat #235 : 학생 챌린지 목록 조회 UI 및 로직 구현

---
+++ android/feature/student/challenge/src/main/kotlin/com/sixkids/student/challeng/history/ChallengeHistoryContract.kt
@@ -0,0 +1,24 @@
+package com.sixkids.student.challeng.history
+
+import androidx.paging.PagingData
+import com.sixkids.model.Challenge
+import com.sixkids.model.RunningChallenge
+import com.sixkids.ui.base.SideEffect
+import com.sixkids.ui.base.UiState
+import kotlinx.coroutines.flow.Flow
+
+data class ChallengeHistoryState(
+    val isLoading: Boolean = false,
+    val runningChallenge: RunningChallenge? = null,
+    val totalChallengeCount: Int = 0,
+    val organizationId: Int = 0,
+    val challengeHistory: Flow<PagingData<Challenge>>? = null
+) : UiState
+
+sealed interface ChallengeHistoryEffect : SideEffect {
+    data class NavigateToChallengeDetail(val challengeId: Long, val groupId: Long? = null) : ChallengeHistoryEffect
+    data class NavigateToCreateGroup(val challengeId: Long) : ChallengeHistoryEffect
+    data class NavigateToJoinGroup(val challengeId: Long) : ChallengeHistoryEffect
+    data object ShowGroupDialog : ChallengeHistoryEffect
+    data class HandleException(val throwable: Throwable, val retry: () -> Unit) : ChallengeHistoryEffect
+}
---
+++ android/feature/student/challenge/src/main/kotlin/com/sixkids/student/challeng/history/ChallengeHistoryViewModel.kt
@@ -0,0 +1,67 @@
+package com.sixkids.student.challeng.history
+
+import androidx.lifecycle.viewModelScope
+import com.sixkids.domain.usecase.challenge.GetChallengeHistoryUseCase
+import com.sixkids.domain.usecase.challenge.GetRunningChallengeUseCase
+import com.sixkids.domain.usecase.organization.GetSelectedOrganizationIdUseCase
+import com.sixkids.ui.base.BaseViewModel
+import com.sixkids.ui.extension.flatMap
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ChallengeHistoryViewModel @Inject constructor(
+    private val getSelectedOrganizationIdUseCase: GetSelectedOrganizationIdUseCase,
+    private val getChallengeHistoryUseCase: GetChallengeHistoryUseCase,
+    private val getRunningChallengeUseCase: GetRunningChallengeUseCase
+) : BaseViewModel<ChallengeHistoryState, ChallengeHistoryEffect>(
+    ChallengeHistoryState()
+) {
+    private var isFirstVisited: Boolean = true
+    private var organizationId: Long = 0
+
+    fun initData() = viewModelScope.launch {
+        if (isFirstVisited.not()) return@launch
+        isFirstVisited = false
+
+        intent { copy(isLoading = true) }
+        getSelectedOrganizationIdUseCase().flatMap { organizationId ->
+            this@ChallengeHistoryViewModel.organizationId = organizationId.toLong()
+            val challengeHistory = getChallengeHistoryUseCase(organizationId)
+            intent { copy(challengeHistory = challengeHistory) }
+            getRunningChallengeUseCase(organizationId)
+        }.onSuccess {
+            intent { copy(isLoading = false, runningChallenge = it) }
+        }.onFailure {
+            when (it) {
+                is NoSuchElementException -> {
+                    intent { copy(isLoading = false, runningChallenge = null) }
+                }
+
+                else -> {
+                    postSideEffect(
+                        ChallengeHistoryEffect.HandleException(it, ::initData)
+                    )
+                }
+            }
+        }
+        intent { copy(isLoading = false) }
+    }
+
+    fun navigateChallengeDetail(challengeId: Long) = postSideEffect(
+        ChallengeHistoryEffect.NavigateToChallengeDetail(challengeId)
+    )
+
+    fun showGroupDialog() = postSideEffect(
+        ChallengeHistoryEffect.ShowGroupDialog
+    )
+
+    fun navigateToCreateGroup() = postSideEffect(
+        ChallengeHistoryEffect.NavigateToCreateGroup(organizationId)
+    )
+
+    fun navigateToJoinGroup() = postSideEffect(
+        ChallengeHistoryEffect.NavigateToJoinGroup(organizationId)
+    )
+}
