06c441f75fc2bd4a73a04739c030c7038ea13c4b
HABIN <dhdudgns80@naver.com>
2024-05-12 00:26:31 +0900

    feat: 멤버 조회 기능 구현 완료
    1. PathVariable 입력으로 들어온 멤버 PK 기준으로 멤버 Simple 정보를 반환합니다.

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/controller/MemberController.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/controller/MemberController.java
@@ -10,7 +10,10 @@ import com.quiz.ourclass.domain.member.dto.request.UpdateFcmTokenRequest;
 import com.quiz.ourclass.domain.member.service.MemberService;
 import com.quiz.ourclass.domain.member.service.client.KakaoOicdClient;
 import com.quiz.ourclass.global.config.annotation.LogExclusion;
+import com.quiz.ourclass.global.dto.MemberSimpleDTO;
 import com.quiz.ourclass.global.dto.ResultResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -116,5 +120,14 @@ public class MemberController implements MemberControllerDocs {
     public ResponseEntity<ResultResponse<?>> deleteMe() {
         memberService.deleteMe();
         return ResponseEntity.ok(ResultResponse.success(null));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResultResponse<?>> select(
+        @Parameter(name = "id", description = "멤버 PK 값", required = true, in = ParameterIn.PATH)
+        @PathVariable(value = "id") Long id
+    ) {
+        MemberSimpleDTO response = memberService.select(id);
+        return ResponseEntity.ok(ResultResponse.success(response));
     }
 }
