fadbebf7a20ee0732e0e88094de6bdf69856c673
차성원 <sungwon4728@naver.com>
2024-05-11 19:43:00 +0900

    docs #220: 태깅인사 스웨거 추가
    (cherry picked from commit 2f2ba7f15ea60e0054d869ad0152d40ef47404aa)

--- backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/request/TagGreetingRequest.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/organization/dto/request/TagGreetingRequest.java
@@ -1,7 +1,12 @@
 package com.quiz.ourclass.domain.organization.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "태깅인사 요청 DTO")
 public record TagGreetingRequest(
+    @Schema(description = "학급 id")
     Long organizationId,
+    @Schema(description = "상대 멤버 id")
     Long memberId
 ) {
 
