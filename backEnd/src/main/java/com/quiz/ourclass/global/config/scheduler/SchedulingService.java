532c4d362df8e8750512723089ef0ae539c8125e
차성원 <sungwon4728@naver.com>
2024-05-11 20:41:29 +0900

    feat #255: 스케줄러 트랜잭션 적용을 위해 transactionTemplate 추가 적용

--- backEnd/src/main/java/com/quiz/ourclass/global/config/scheduler/SchedulingService.java
+++ backEnd/src/main/java/com/quiz/ourclass/global/config/scheduler/SchedulingService.java
@@ -7,15 +7,17 @@ import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RequiredArgsConstructor
 @Service
 public class SchedulingService {
 
     private final TaskScheduler taskScheduler;
+    private final TransactionTemplate transactionTemplate;
 
     public <T> void scheduleTask(T target, Consumer<T> action, LocalDateTime executionTime) {
-        SchedulingTask<T> task = new SchedulingTask<>(target, action);
+        SchedulingTask<T> task = new SchedulingTask<>(target, action, transactionTemplate);
         Instant executionDate = executionTime.atZone(ZoneId.systemDefault()).toInstant();
 
         taskScheduler.schedule(task, executionDate);
--- backEnd/src/main/java/com/quiz/ourclass/global/config/scheduler/SchedulingTask.java
+++ backEnd/src/main/java/com/quiz/ourclass/global/config/scheduler/SchedulingTask.java
@@ -2,15 +2,20 @@ package com.quiz.ourclass.global.config.scheduler;
 
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @AllArgsConstructor
 public class SchedulingTask<T> implements Runnable {
 
     private final T target;
     private final Consumer<T> task;
+    private final TransactionTemplate transactionTemplate;
 
     @Override
     public void run() {
-        task.accept(target);
+        transactionTemplate.execute(status -> {
+            task.accept(target);
+            return null;
+        });
     }
 }
