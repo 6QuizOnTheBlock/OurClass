e6c0e6446c2dd407e7323a9545ac61f393d177d7
정철주 <cjfwn96@gmail.com>
2024-05-13 03:29:55 +0900

    feat #303: StudentHomeMainViewModel 유저정보 조회

--- android/feature/student/home/src/main/java/com/sixkids/student/home/main/StudentHomeMainViewModel.kt
+++ android/feature/student/home/src/main/java/com/sixkids/student/home/main/StudentHomeMainViewModel.kt
@@ -1,12 +1,31 @@
 package com.sixkids.student.home.main
 
+import androidx.lifecycle.viewModelScope
+import com.sixkids.domain.usecase.user.LoadUserInfoUseCase
 import com.sixkids.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StudentHomeMainViewModel @Inject constructor(
+    private val loadUserInfoUseCase: LoadUserInfoUseCase
+) : BaseViewModel<StudentHomeMainState, StudentHomeMainEffect>(StudentHomeMainState()) {
 
-) : BaseViewModel<StudentHomeMainState, StudentHomeMainEffect>(StudentHomeMainState()){
-
+    fun loadUserInfo() {
+        viewModelScope.launch {
+            loadUserInfoUseCase()
+                .onSuccess { userinfo ->
+                    intent {
+                        copy(
+                            studentName = userinfo.name,
+                            studentImageUrl = userinfo.photo,
+                        )
+                    }
+                }
+                .onFailure {
+                     postSideEffect(StudentHomeMainEffect.onShowSnackBar(it.message?:"회원정보 호출에 실패했습니다."))
+                }
+        }
+    }
 }
