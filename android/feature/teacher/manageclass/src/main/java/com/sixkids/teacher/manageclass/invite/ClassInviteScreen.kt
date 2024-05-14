55f936b43e8cacece14d9af486c9f51e416ddbc9
정철주 <cjfwn96@gmail.com>
2024-05-14 18:01:48 +0900

    create #323: 학급초대 페이지 생성

---
+++ android/feature/teacher/manageclass/src/main/java/com/sixkids/teacher/manageclass/invite/ClassInviteScreen.kt
@@ -0,0 +1,24 @@
+package com.sixkids.teacher.manageclass.invite
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ClassInviteRoute(
+
+) {
+
+}
+
+@Composable
+fun ClassInviteScreen(
+
+) {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ClassInviteScreenPreview() {
+    ClassInviteScreen()
+}
