96445b4a05e72d353068aa531604a3073d07d5ac
정철주 <cjfwn96@gmail.com>
2024-05-15 16:57:11 +0900

    fix #320: 설정 화면 취소, 확인 버튼 추가

--- android/feature/teacher/manageclass/src/main/java/com/sixkids/teacher/manageclass/setting/ClassSettingScreen.kt
+++ android/feature/teacher/manageclass/src/main/java/com/sixkids/teacher/manageclass/setting/ClassSettingScreen.kt
@@ -2,11 +2,13 @@ package com.sixkids.teacher.manageclass.setting
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +16,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sixkids.designsystem.component.appbar.UlbanDetailAppBar
+import com.sixkids.designsystem.component.button.UlbanFilledButton
 import com.sixkids.designsystem.theme.Red
+import com.sixkids.designsystem.theme.RedDark
 import com.sixkids.designsystem.theme.UlbanTypography
 import com.sixkids.teacher.manageclass.R
 import com.sixkids.teacher.manageclass.setting.component.SimpleOutlinedTextField
@@ -31,6 +35,8 @@ fun ClassSettingRoute(
 fun ClassSettingScreen(
     modifier: Modifier = Modifier,
     classSettingState: ClassSettingState = ClassSettingState(),
+    cancelButtonOnClick: () -> Unit = {},
+    confirmButtonOnClick: () -> Unit = {}
 ) {
     Column(
         modifier = modifier.fillMaxSize()
@@ -81,7 +87,22 @@ fun ClassSettingScreen(
                     .fillMaxWidth()
                     .padding(4.dp),
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(30.dp))
+            Row {
+                UlbanFilledButton(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(id = R.string.cancel),
+                    onClick = cancelButtonOnClick,
+                    color = Red,
+                    textColor = RedDark
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                UlbanFilledButton(
+                    modifier = Modifier.weight(1f),
+                    text = stringResource(id = R.string.ok),
+                    onClick = cancelButtonOnClick,
+                )
+            }
         }
     }
 }
