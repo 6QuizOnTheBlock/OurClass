5f888918fef4c65be706cf4165942ed7318b6630
JeonSooMin <wjsaos2081@gmail.com>
2024-05-19 14:23:34 +0900

    feat: OIDC 토큰 설정

--- backEnd/src/main/java/com/quiz/ourclass/domain/member/repository/querydsl/MemberRepositoryQuerydsl.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/repository/querydsl/MemberRepositoryQuerydsl.java
@@ -2,10 +2,12 @@ package com.quiz.ourclass.domain.member.repository.querydsl;
 
 import com.quiz.ourclass.domain.member.dto.response.MemberMeResponse;
 import com.quiz.ourclass.domain.member.entity.Member;
+import java.util.List;
 
 public interface MemberRepositoryQuerydsl {
 
-
     public MemberMeResponse rememberMe(Member me, long orgId);
+
+    public List<Member> getOrgMembers(long orgId);
 
 }
--- backEnd/src/main/java/com/quiz/ourclass/domain/member/service/client/KakaoOicdClient.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/member/service/client/KakaoOicdClient.java
@@ -2,17 +2,19 @@ package com.quiz.ourclass.domain.member.service.client;
 
 
 import com.quiz.ourclass.domain.member.dto.response.OIDCPublicKeysResponse;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*
-* (name : 실제 구현체가 Application Context에 빈으로 등록될 때 이름
-*  url  : 요청을 보낼 EndPoint)
-* */
+ * (name : 실제 구현체가 Application Context에 빈으로 등록될 때 이름
+ *  url  : 요청을 보낼 EndPoint)
+ * */
 @FeignClient(name = "kakao-oicd-client", url = "https://kauth.kakao.com")
 public interface KakaoOicdClient {
 
     // 스프링 어노테이션으로 요청 보내기 가능
+    @Cacheable(value = "OIDC")
     @GetMapping("/.well-known/jwks.json")
     OIDCPublicKeysResponse getKakaoOIDCOpenKeys();
 
