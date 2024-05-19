4da796fd62b1e2bb28a2d242a096b4c4df78d05e
ghddbwns9808 <ghddbwns9808@gmail.cocm>
2024-05-19 17:46:32 +0900

    design #372: teacher manage student main

--- android/feature/teacher/managestudent/src/main/java/com/sixkids/teacher/managestudent/main/ManageStudentMainViewModel.kt
+++ android/feature/teacher/managestudent/src/main/java/com/sixkids/teacher/managestudent/main/ManageStudentMainViewModel.kt
@@ -3,6 +3,7 @@ package com.sixkids.teacher.managestudent.main
 import androidx.lifecycle.viewModelScope
 import com.sixkids.domain.usecase.organization.GetOrganizationMemberUseCase
 import com.sixkids.domain.usecase.organization.GetSelectedOrganizationIdUseCase
+import com.sixkids.domain.usecase.organization.LoadSelectedOrganizationNameUseCase
 import com.sixkids.ui.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -11,10 +12,17 @@ import javax.inject.Inject
 @HiltViewModel
 class ManageStudentMainViewModel @Inject constructor(
     private val getSelectedOrganizationIdUseCase: GetSelectedOrganizationIdUseCase,
-    private val getOrganizationMemberUseCase: GetOrganizationMemberUseCase
+    private val getOrganizationMemberUseCase: GetOrganizationMemberUseCase,
+    private val loadSelectedOrganizationNameUseCase: LoadSelectedOrganizationNameUseCase
 ): BaseViewModel<ManageStudentMainState, ManageStudentMainEffect>(ManageStudentMainState()){
     fun initData(){
         viewModelScope.launch {
+            loadSelectedOrganizationNameUseCase().onSuccess {
+                intent { copy(classString = it) }
+            }.onFailure {
+                intent { copy(classString = "") }
+            }
+
             getSelectedOrganizationIdUseCase().onSuccess {
                 getOrganizationMemberUseCase(it)
                     .onSuccess {
