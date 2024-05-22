f4b4986f2e9ea3719dde6afb85059e972d9b4850
JeonSooMin <wjsaos2081@gmail.com>
2024-05-22 12:45:48 +0900

    chore: StreamingController 현재 유저 동기화 기능 잠시 삭제

--- backEnd/src/main/java/com/quiz/ourclass/domain/quiz/controller/StreamingController.java
+++ backEnd/src/main/java/com/quiz/ourclass/domain/quiz/controller/StreamingController.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,15 +40,5 @@ public class StreamingController {
         @Header("Authorization") final String accessToken) {
         streamingService.sendAnswer(request, accessToken);
     }
-
-    // 3. 서버 <-> 클라이언트 시간 동기화 (React 화면 마운트 시에 서버에 요청, 서버는 남은 카운트 다운 수 전송)
-    @MessageMapping("/current")
-    @SendToUser("/queue/time") // 이 어노테이션은 요청을 보낸 클라이언트의 해당 주소로만 값을 보냄!
-    public String currentTime(StompHeaderAccessor sha) {
-        log.info("여기까지 왔다!={}", sha.getSessionId());
-        log.info("Destination={}", sha.getDestination());
-        return String.valueOf(countdownService.getCurrentCountDown());
-    }
-
 
 }
